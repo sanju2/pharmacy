a54b9218f3d299ec71fd8465f3830563f8d2b5bb
lasantha96 <lsanjeewa947@gmail.com>
2022-09-02 21:42:55 +0400

    Pop=up added for confirm box

--- resources/views/pharmacy/index.blade.php
+++ resources/views/pharmacy/index.blade.php
@@ -37,11 +37,31 @@
                 <td>{{$pharmacy->category}}</td>
                 <td>{{$pharmacy->price}}</td>
                 <td><a href="{{route('pharmacy.edit',$pharmacy->id)}}"><button class="btn btn-primary">Edit</button></a></td>
-                <td><button class="btn btn-danger">Delete</button></td>
+                <td><button class="btn btn-danger" data-toggle="modal" data-target="#exampleModal{{ $pharmacy->id }}">Delete</button></td>
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal{{ $pharmacy->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Delete Confirm</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div class="modal-body">
+                        Are you sure ?
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-danger">Delete</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </tr>
               @endforeach
 
-              @else 
+              @else
               <p>No Medicine to show</p>
               @endif
 
